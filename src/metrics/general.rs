d8b21c8adec336dfe45c53abb2b88fb10766a8ab
Isaac Whitfield <iw@whitfin.io>
2018-08-02 14:14:26 -0700

    Add leaf directory count to report output

--- src/metrics/general.rs
+++ src/metrics/general.rs
@@ -1,13 +1,16 @@
 //! General metrics tracking for S3 objects.
 use humantime;
 use rusoto_s3::Object;
+use std::path::Path;
 use std::time::{Duration, SystemTime};
 
 use super::Metric;
 
 /// Container struct for general metrics tracked by S3.
 pub struct General {
+    latest_dir: String,
     start_time: SystemTime,
+    total_dirs: u64,
     total_keys: u64,
     total_size: u64,
 }
@@ -17,7 +20,9 @@ impl Metric for General {
     /// Constructs a new `General` struct.
     fn new() -> General {
         General {
+            latest_dir: "s3-meta-start".to_string(),
             start_time: SystemTime::now(),
+            total_dirs: 0,
             total_keys: 0,
             total_size: 0,
         }
@@ -25,8 +30,21 @@ impl Metric for General {
 
     /// Registers an S3 `Object` with this metric struct.
     fn register(&mut self, object: &Object) {
+        let key = super::get_key(object);
+        let dir = match Path::new(&key).file_name() {
+            Some(file) => &key[0..(key.len() - file.len())],
+            None => key,
+        };
+
         self.total_keys += 1;
         self.total_size += super::get_size(object);
+
+        if self.latest_dir == dir {
+            return;
+        }
+
+        self.latest_dir = dir.to_string();
+        self.total_dirs += 1;
     }
 
     /// Prints out all internal statistics under the `general` header.
@@ -46,5 +64,6 @@ impl Metric for General {
         ::util::log_pair("total_time", humantime::format_duration(task_duration));
         ::util::log_pair("total_space", ::util::convert_bytes(self.total_size));
         ::util::log_pair("total_files", ::util::comma(self.total_keys));
+        ::util::log_pair("total_folders", ::util::comma(self.total_dirs));
     }
 }
